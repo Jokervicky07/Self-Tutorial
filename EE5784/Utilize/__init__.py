d2310312c12e8e551aaeb923b3f15b8d3ebb4480
SHEN JIAHUI <jokervicky07@gmail.com>
2025-05-18 17:57:05 +0800

    Update

--- EE5784/Utilize/__init__.py
+++ EE5784/Utilize/__init__.py
@@ -2,12 +2,14 @@ from .CrossEntropy import CrossEntropy
 from .FocalLoss import FocalLoss
 from .Conformer import Conformer
 from .SelfAttentionPooling import SelfAttentionPooling
-from .AMSoftmax import AMSoftmax
+from .AMSoftmaxLoss import AMSoftmaxLoss
+from .CosineLinear import CosineLinear
 
 __all__ = [
     "CrossEntropy",
     "FocalLoss",
     "Conformer",
     "SelfAttentionPooling",
-    "AMSoftmax"
+    "AMSoftmaxLoss",
+    "CosineLinear",
 ]
